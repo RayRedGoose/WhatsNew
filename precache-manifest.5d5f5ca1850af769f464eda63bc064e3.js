ff04030dfd32032689cb863e4fdf328bd368ce73
Raisa Primerova <riyuhide@gmail.com>
2020-02-10 23:00:30 -0700

    Updates

---
+++ precache-manifest.5d5f5ca1850af769f464eda63bc064e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845b9dbd53f518698c271010746439a1",
+    "url": "/WhatsNew/index.html"
+  },
+  {
+    "revision": "96ab217297cee9220820",
+    "url": "/WhatsNew/static/css/main.dd1d663c.chunk.css"
+  },
+  {
+    "revision": "dcd5b0e9dd6addcafcc4",
+    "url": "/WhatsNew/static/js/2.b5f72e8b.chunk.js"
+  },
+  {
+    "revision": "96ab217297cee9220820",
+    "url": "/WhatsNew/static/js/main.9cf039df.chunk.js"
+  },
+  {
+    "revision": "51f9c454f1a25c8ee577",
+    "url": "/WhatsNew/static/js/runtime-main.43992c84.js"
+  },
+  {
+    "revision": "4df22423dbb2defa7bf5d6a63e657015",
+    "url": "/WhatsNew/static/media/arrow.4df22423.svg"
+  },
+  {
+    "revision": "826c4c40f99607879f350dadd1f42316",
+    "url": "/WhatsNew/static/media/health.826c4c40.svg"
+  },
+  {
+    "revision": "acefea4c5002f7009ed6c80c9eddaa30",
+    "url": "/WhatsNew/static/media/placeholder.acefea4c.svg"
+  },
+  {
+    "revision": "e94a8beb20b674f13566afa6ac3940d9",
+    "url": "/WhatsNew/static/media/rocket.e94a8beb.svg"
+  },
+  {
+    "revision": "f42b474de992efdd49072efe5f48bb82",
+    "url": "/WhatsNew/static/media/technology.f42b474d.svg"
+  },
+  {
+    "revision": "a680dc40cf07b01baa0ff79e10e66445",
+    "url": "/WhatsNew/static/media/tickets.a680dc40.svg"
+  }
+]);
